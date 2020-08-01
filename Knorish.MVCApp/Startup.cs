6e31f5c3b8a4c91c9fdc4622ee6f3c3391e84f9c
apnadost <rishi.apnadost@gmail.com>
2020-08-01 21:49:23 +0530

    test

---
+++ Knorish.MVCApp/Startup.cs
@@ -0,0 +1,66 @@
+using Knorish.Infrastructure;
+using Knorish.Service;
+using Knorish.Service.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Knorish.MVCApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddEntityFrameworkSqlServer();
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            
+            services.AddScoped<IKnorishService, KnorishService>();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+               
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ Knorish.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Knorish.WebApplication.Models;
+using Knorish.Service.Interfaces;
+using Knorish.Infrastructure;
+using Knorish.Dto;
+
+namespace Knorish.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IKnorishService _service;
+        public HomeController(ILogger<HomeController> logger, IKnorishService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        public  ActionResult Index()
+        {            
+            return View();
+        }
+        [HttpPost]
+        public async Task<JsonResult> AddBoat(Boat boat)
+        {
+            CommonDto commonDto = new CommonDto();            
+            commonDto=await _service.RegisterBoat(boat);            
+            return Json(commonDto);
+        }
+        
+        public async Task<JsonResult> GetBoatList()
+        {
+            return Json(await _service.GetBoatList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Boat()
+        {
+            return View();
+        }
+        public IActionResult BoatBooking()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
